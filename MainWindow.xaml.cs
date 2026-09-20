fd4a1391d9f27d0061be45901a3427211cab2160
Shizuki Akiyama <shizuki_mobile@ymail.ne.jp>
2026-09-21 00:57:30 +0900

    Add UI and clean generated files from repository

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SongInfoViewer
 {
@@ -19,6 +16,61 @@ namespace SongInfoViewer
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _songTitle = "文化祭オープニングテーマ";
+        private string _artistName = "SongInfoViewer Band";
+        private string _albumTitle = "School Festival 2026 Selection";
+        private ImageSource? _albumArt = null;
+
+        public string SongTitle
+        {
+            get => _songTitle;
+            set { _songTitle = value; OnPropertyChanged(); }
+        }
+
+        public string ArtistName
+        {
+            get => _artistName;
+            set { _artistName = value; OnPropertyChanged(); }
+        }
+
+        public string AlbumTitle
+        {
+            get => _albumTitle;
+            set { _albumTitle = value; OnPropertyChanged(); }
+        }
+
+        public ImageSource? AlbumArt
+        {
+            get => _albumArt;
+            set { _albumArt = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public static readonly NullToVisibilityConverter Instance = new();
+
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
